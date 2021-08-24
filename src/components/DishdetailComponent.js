67ac402391b986431dae20fc8b9c117dde3196f2
Sheba Ameri <saba.aemri1378@gmail.com>
2021-08-24 12:13:52 +0430

    Week 3 Assignment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,14 +1,142 @@
 
 import React from 'react';
 import { Card, CardImg, CardText, CardBody,
-  CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+  CardTitle, Breadcrumb, BreadcrumbItem, Col, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Component } from 'react';
+import {  Collapse, Jumbotron,
+  Button, Modal, ModalHeader, ModalBody,
+  Form, FormGroup, Input, Label } from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 
-// class DishDetail  extends Component {
-//     constructor(props) {
-//         super(props);
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+const isNumber = (val) => !isNaN(Number(val));
+const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
+  
+  class CommentForm extends Component {
+  constructor(props) {
+    super(props);
 
-//     };
+    this.toggleNav = this.toggleNav.bind(this);
+    this.state = {
+      isNavOpen: false,
+      isModalOpen: false
+
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+
+
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+  }
+  handleLogin(event) {
+    this.toggleModal();
+    alert("Username: " + this.username.value + " Password: " + this.password.value
+        + " Remember: " + this.remember.checked);
+    event.preventDefault();
+
+}
+
+  toggleNav() {
+    this.setState({
+      isNavOpen: !this.state.isNavOpen
+    });
+  }
+
+
+  render() {
+    return( 
+      <div>   
+        <Button outline onClick={this.toggleModal} > Submit Comment</Button>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                    <Form onSubmit={this.handleSubmit}>
+
+                      <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                      
+                      <Row className="form-group">
+                        <Col>
+                          <Label htmlFor="firstname" md={2}>Rating </Label>
+
+                            <Control.select model=".contactType" name="contactType"
+                                className="form-control">
+                                <option>5</option>
+                                <option>4</option>
+                                <option>3</option>
+                                <option>2</option>
+                                <option>1</option>
+                            </Control.select>
+                                                    
+                        </Col>
+      
+                      </Row>
+
+
+                      <Row className="form-group">
+                        <Col>
+                        <Label htmlFor="firstname" md={4}>Your Name</Label>
+                              
+                              <Control.text model=".firstname" id="firstname" name="firstname"
+                                  placeholder="First Name"
+                                  className="form-control"
+                                  validators={{
+                                      required, minLength: minLength(3), maxLength: maxLength(15)
+                                  }}
+                                  />
+                              <Errors
+                                  className="text-danger"
+                                  model=".firstname"
+                                  show="touched"
+                                  messages={{
+                                      required: 'Required',
+                                      minLength: 'Must be greater than 2 characters',
+                                      maxLength: 'Must be 15 characters or less'
+                                  }}
+                              />
+                          
+                        </Col>
+ 
+                          </Row>
+                          <Row className="form-group">
+                                <Col>
+                                <Label htmlFor="message" md={2}>Comment</Label>
+
+                                <Control.textarea model=".message" id="message" name="message"
+                                    rows="6"
+                                    className="form-control" />
+                                
+                                </Col>
+                            
+
+                            </Row>
+
+
+
+                          <Row className="form-group">
+                              <Col md={{size:10}}>
+                                  <Button type="submit" color="primary">
+                                  Submit
+                                  </Button>
+                              </Col>
+                          </Row>
+                      </LocalForm>
+
+                      </Form>
+                    </ModalBody>
+                </Modal>
+      </div>
+      )
+
+  }
+}
 
     function RenderDish({dish}) {
         if (dish != null)
@@ -26,38 +154,6 @@ import { Link } from 'react-router-dom';
               <div></div>
             )
       }
-    // function RenderComments({dish}) {
-        
-    //     if(dish != null)
-    //       {
-            
-    //         return(
-    //         <div>
-    //           <div>
-    //           <h4> Comments</h4>
-    //           </div>
-    //           {
-    //             //dish.comments[0].comment
-    //             // this.props.category
-    //             dish.comments.map((showCM)=> (
-    //               <div> 
-    //                 {showCM.comment}
-                   
-    //                 <p>
-    //                 --{showCM.author}
-    //                 &nbsp;
-    //                   {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(showCM.date)))}
-    //                 </p>
-    //               </div>
-
-    //             ))
-    //           }
-    //         </div>
-    //       )}else
-    //         return(
-    //             <div>there's no comment</div>
-    //         )
-    // }
     function RenderComments({comments}){
       return(
         <div>
@@ -70,9 +166,8 @@ import { Link } from 'react-router-dom';
                 <p>
                   --{eachComment.author}
                   &nbsp;
-                  {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(eachComment.date)))}
- 
-
+                  {new Intl.DateTimeFormat('en-US',
+                   { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(eachComment.date)))} 
                 </p>
               </div>
             )
@@ -80,7 +175,7 @@ import { Link } from 'react-router-dom';
            )
           
           }
-
+          <CommentForm />
         </div>
       )
     }
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -71,26 +71,32 @@ class Header extends Component {
                 <Navbar dark expand="md">
                     <div className="container">
                         <NavbarToggler onClick={this.toggleNav} />
-                        <NavbarBrand className="mr-auto" href="/"><img src='https://toppng.com/uploads/preview/fast-food-11549470764zfdstlnivq.png'height="30" width="41" alt='Ristorante Con Fusion' /></NavbarBrand>
+                        <NavbarBrand className="mr-auto" href="/">
+                            <img src='https://toppng.com/uploads/preview/fast-food-11549470764zfdstlnivq.png'height="30" width="41" alt='Ristorante Con Fusion' /></NavbarBrand>
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
                             <NavItem>
-                                <NavLink className="nav-link"  to='/home'><span className="fa fa-home fa-lg"></span> Home</NavLink>
+                                <NavLink className="nav-link"  to='/home'>
+                                    <span className="fa fa-home fa-lg"></span> Home</NavLink>
                             </NavItem>
                             <NavItem>
-                                <NavLink className="nav-link" to='/aboutus'><span className="fa fa-info fa-lg"></span> About Us</NavLink>
+                                <NavLink className="nav-link" to='/aboutus'>
+                                    <span className="fa fa-info fa-lg"></span> About Us</NavLink>
                             </NavItem>
                             <NavItem>
-                                <NavLink className="nav-link"  to='/menu'><span className="fa fa-list fa-lg"></span> Menu</NavLink>
+                                <NavLink className="nav-link"  to='/menu'>
+                                    <span className="fa fa-list fa-lg"></span> Menu</NavLink>
                             </NavItem>
                             <NavItem>
-                                <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
+                                <NavLink className="nav-link" to='/contactus'>
+                                    <span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
                             </NavItem>
                             </Nav>
                             
                             <Nav className="ml-auto" navbar>
                                 <NavItem>
-                                    <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
+                                    <Button outline onClick={this.toggleModal}>
+                                        <span className="fa fa-sign-in fa-lg"></span> Login</Button>
                                 </NavItem>
                             </Nav>
                         </Collapse>
