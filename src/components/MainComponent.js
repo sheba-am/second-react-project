3a323c67dd2a80b11e4b0561d9f54c4d45b31508
Sheba Ameri <saba.aemri1378@gmail.com>
2021-08-28 21:54:53 +0430

    React Animation V2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -102,17 +102,6 @@ class Main extends Component {
             </CSSTransition>
           </TransitionGroup>
           
-
-        <div>
-          <Switch>
-              <Route path='/home' component={HomePage} />
-              <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />
-              <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
-              <Route path='/menu/:dishId' component={DishWithId} />
-              <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
-              <Redirect to="/home" />
-          </Switch>
-        </div>
         <Footer />
       </div>
     );
